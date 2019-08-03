c469adb7387db99812d06b2fb00c6110635dff6c
unknown <dehme.armenio@gmail.com>
2019-08-03 07:40:29 -0300

    add basic route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Index() {
+  return <h2>Genero</h2>;
+}
+
+function About() {
+  return <h2>Tipo</h2>;
+}
+
+function Users() {
+  return <h2>Avatar</h2>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about/">About</Link>
+            <Link to="/users/">Users</Link>
+          </nav>
+
+          <Route path="/" exact component={Index} />
+          <Route path="/about/" component={About} />
+          <Route path="/users/" component={Users} />
+        </div>
+      </Router>
     </div>
   );
 }
