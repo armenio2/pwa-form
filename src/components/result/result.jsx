578c2a29a439f92d2fc24a52d2281bc6c52fd3f2
unknown <dehme.armenio@gmail.com>
2019-08-03 14:39:32 -0300

    add img result

--- src/components/result/result.jsx
+++ src/components/result/result.jsx
@@ -5,6 +5,9 @@ import useAxios from 'axios-hooks'
 
 const Result = (props) => {
     const [{ data },] = useAxios('https://viacep.com.br/ws/05361050/json/')
+    const jedi = <img alt='foo' src={'https://vignette.wikia.nocookie.net/pt.starwars/images/5/59/ThreeJedi.jpg/revision/latest?cb=20100921170459'} />
+    const sith = <img alt='foo' src={'https://i2.wp.com/sociedadejedi.com.br/wp-content/uploads/2016/12/received_1800610233544302.jpeg?fit=700%2C700&ssl=1'} />
+
     return (
         <div style={containerStyle} className="container-fluid">
             <Head title={props.title} />
@@ -12,6 +15,9 @@ const Result = (props) => {
                 <div className="row  align-self-center">
                     <div className="col-8">
                         <span style={spanStyle}>Parabens você escolheu o lado {props.side || 'Neutro'} da força!!! </span>
+                    </div>
+                    <div className="col-8">
+                        {props.side === 'Sith' ? sith : jedi}
                     </div>
                 </div>
             </div>
